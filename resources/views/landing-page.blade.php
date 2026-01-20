6e3f71156c80558c1020dbd872036dd8fb3ab224
wira dinata <permanawiradinata27@gmail.com>
2026-01-20 21:42:31 +0800

    add authentication

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -83,9 +83,24 @@
                                         <li class="nav-item">
                                             <a class="page-scroll" href="#contact">Contact</a>
                                         </li>
-                                        <li class="nav-item">
-                                            <a class="page-scroll" href="">Login</a>
-                                        </li>
+
+                                        @if (Route::has('login'))
+                                        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                                            @auth
+                                            <li class="nav-item">
+                                                <a class="page-scroll" href="{{ url('/dashboard') }}">Dashboard</a>
+                                            </li>
+                                            @else
+                                            <li class="nav-item">
+                                                <a class="page-scroll" href="{{ route('login') }}">Login</a>
+
+                                                @if (Route::has('register'))
+                                                <a class="page-scroll" href="{{ route('register') }}">Register</a>
+                                            </li>
+                                            @endif
+                                            @endauth
+                                        </div>
+                                        @endif
                                     </ul>
                                 </div>
 
